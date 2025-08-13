a0053262f0119a3858cfef8abf1b13a4b5417a49
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-08-13 15:57:09 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,44 @@
+
+const mongoose = require("mongoose"); 
+const Schema = mongoose.Schema; 
+
+const listingSchema = new Schema({
+  title: {
+    type: String, 
+    required: true, 
+  },
+  description: String, 
+  image: {
+    filename: String, 
+    url: {
+      type: String, 
+      default: 
+        "https://unsplash.com/photos/a-patio-with-tables-and-chairs-tgp5RtxfHIQ",
+    
+      set: (v) => 
+        v === ""
+          ?"https://unsplash.com/photos/a-patio-with-tables-and-chairs-tgp5RtxfHIQ"
+          : v,
+    }
+  },
+  
+  price: Number, 
+  location: String, 
+  country: String, 
+
+}); 
+
+const Listing = mongoose.model("Listing", listingSchema); 
+module.exports = Listing; 
+
+
+
+
+
+
+
+
+
+
+
+
