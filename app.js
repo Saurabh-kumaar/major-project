8413012ba9e1687d8f963122db6594b18764c33d
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-08-29 23:28:28 +0530

    Add

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.delete(
   console.log(deletedListing); 
   res.redirect("/listings"); 
 }));
-
+daf 
 qer 
 // it is for all error-----------------------------------------------------------------------
 app.use((req, res, next) => {                       // || app.all("*", (req, res, next) => {
