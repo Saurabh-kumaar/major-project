037b3aa35c4fbbd3456ec74e64d0a13c1840bda5
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-09-15 16:32:59 +0530

    Add

--- app.js
+++ app.js
@@ -12,10 +12,15 @@ const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport"); 
+const LocalStrategy = require("passport-local"); 
+const User = require("./models/user.js"); 
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js"); 
+
  
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"; 
 
@@ -58,9 +63,17 @@ app.get("/", (req, res) => {
   res.send("Hii, I am root"); 
 }); 
 
-
 app.use(session(sessionOptions));
 app.use(flash());  
+
+app.use(passport.initialize());
+app.use(passport.session()); 
+passport.use(new LocalStrategy(User.authenticate()));      // for login and signup 
+
+
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
@@ -68,16 +81,29 @@ app.use((req, res, next) => {
   next(); 
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+//demo signup
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com", 
+//     username: "delta-student",
+//   }); 
+
+//   let registerUser = await User.register(fakeUser, "helloworld");
+//   res.send(registeredUser); 
+// });
+
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter); 
+
 
 // it is for all error-----------------------------------------------------------------------
 app.use((req, res, next) => {                       // || app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found"))   // ||  next (new ExpressError(404, "page not found"))})
 });
 
-
-
+ 
 // middleware for error handling 
 app.use((err, req, res, next) => {
   const {statusCode = 500, message = "something went wrong"} = err;
