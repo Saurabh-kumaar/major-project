fea4e972c9be456bb9c9f5454c0ba7868a6746c9
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-11-25 10:32:08 +0530

    Add

---
+++ public/js/map.js
@@ -0,0 +1,13 @@
+
+  
+    mapboxgl.accessToken =  mapToken; 
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        // choose from mapbox's core style, or map 
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [77.2090, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        zoom: 9 // starting zoom
+    }); 
+
+ 
